1628c6d7f2b8db6ac00eb96f335f900596db031d
Vadim Saprykin <sprkweb@yandex.ru>
2021-07-20 20:49:22 +0300

    Login and Logout GQL Mutations

--- commenti_server/schema.py
+++ commenti_server/schema.py
@@ -1,7 +1,9 @@
 import graphene
 from graphene import relay
 import graphene_django
-
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+from django.contrib import auth
 from django.contrib.auth.models import User
 from comments.models import Comment
 
@@ -28,6 +30,40 @@ class CommentGQLNode(graphene_django.DjangoObjectType):
         )
         interfaces = (relay.Node, )
 
+class GQLLogin(graphene.Mutation):
+    class Input:
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+
+    user = graphene.Field(UserGQLType)
+
+    @classmethod
+    def mutate(cls, root, info, username, password):
+        user = auth.authenticate(
+            info.context,
+            username=username,
+            password=password
+        )
+        if user is not None:
+            auth.login(info.context, user)
+            return GQLLogin(user=user)
+        else:
+            raise ValidationError(
+                _('Incorrect username or password'),
+                code='invalid login'
+            )
+
+class GQLLogout(graphene.Mutation):
+    class Input:
+        pass
+
+    ok = graphene.Boolean()
+
+    @classmethod
+    def mutate(cls, root, info):
+        auth.logout(info.context)
+        return GQLLogout(ok=True)
+
 class Query(graphene.ObjectType):
     comment = relay.Node.Field(CommentGQLNode)
 
@@ -35,5 +71,8 @@ class Query(graphene.ObjectType):
     def resolve_comments(root, info, page, **args):
         return Comment.objects.filter(page=page, parent=None)
 
+class Mutation(graphene.ObjectType):
+    login = GQLLogin.Field()
+    logout = GQLLogout.Field()
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
